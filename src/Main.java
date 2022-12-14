903b80f4cac4945f89642a44350933c0a6073a07
emreyilmaz1912 <emreyilmaz264@gmail.com>
2022-12-14 13:06:49 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Üslü Sayı Hesaplamaya Hoşgeldiniz!");
+        Scanner input = new Scanner(System.in);
+        int n,k,total = 1;
+        System.out.print("Üssü alınacak sayıyı giriniz :");
+        n = input.nextInt();
+        System.out.print("Sayının üssünü giriniz :");
+        k = input.nextInt();
+
+        for (int i =1; i <= k; i++){
+            total *= n;
+
+        }
+        System.out.println("Cevap :" + total);
     }
 }
